41fb02311279dba6135557d2140e83139c350f84
Haitham Aouati <55059470+haithamaouati@users.noreply.github.com>
2023-11-15 14:48:17 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let secretNumber = Math.floor(Math.random() * 100) + 1;
 let guesses = [];
 let attempts = 0;
+const maxAttempts = 10;
 
 function checkGuess() {
   let userGuess = parseInt(document.getElementById('guess').value);
@@ -13,14 +14,48 @@ function checkGuess() {
   guesses.push(userGuess);
 
   if (userGuess === secretNumber) {
-    document.getElementById('result').innerHTML = `Congratulations! You guessed the number ${secretNumber} in ${attempts} attempts.`;
-    document.getElementById('guesses').innerHTML = `No. Of Guesses: ${attempts}<br>Guessed Numbers are: ${guesses.join(', ')}`;
-    document.getElementById('guess').disabled = true;
+    showVictoryMessage();
   } else {
     let hint = userGuess < secretNumber ? 'Too low. Try again.' : 'Too high. Try again.';
     document.getElementById('result').innerHTML = hint;
     document.getElementById('guesses').innerHTML = `No. Of Guesses: ${attempts}<br>Guessed Numbers are: ${guesses.join(', ')}`;
+
+    if (attempts === maxAttempts) {
+      showLossMessage();
+    }
   }
+}
+
+function showVictoryMessage() {
+  alert(`Congratulations! You guessed the number ${secretNumber} in ${attempts} attempts.`);
+  askForReload();
+}
+
+function showLossMessage() {
+  alert(`Sorry, you couldn't guess the number in ${maxAttempts} attempts. The correct number was ${secretNumber}.`);
+  askForReload();
+}
+
+function askForReload() {
+  if (confirm("Do you want to play again?")) {
+    resetGame();
+  } else {
+    disableInput();
+  }
+}
+
+function resetGame() {
+  secretNumber = Math.floor(Math.random() * 100) + 1;
+  guesses = [];
+  attempts = 0;
+  document.getElementById('result').innerHTML = '';
+  document.getElementById('guesses').innerHTML = 'No. Of Guesses: 0<br>Guessed Numbers are: None';
+  document.getElementById('guess').value = '';
+  document.getElementById('guess').disabled = false;
+}
+
+function disableInput() {
+  document.getElementById('guess').disabled = true;
 }
 
 mermaid.initialize({startOnLoad:true});
